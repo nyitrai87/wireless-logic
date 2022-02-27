a48bcc020e7e1c7a2ed29c4e933f650fde1f54d5
Adam Nyitrai <adam.nyitrai87@gmail.com>
2022-02-27 21:00:34 +0000

    chore: refactoring for better testability

--- index.js
+++ index.js
@@ -1,34 +1,67 @@
 import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
 
-fetch('https://demo.comesconnected.com/')
-    .then(res => res.text())
-    .then(html => {
-        const $ = cheerio.load(html);
-        const result = [];
-        const packages = $('.package');
+const url = 'https://demo.comesconnected.com/'
+const selectors = {
+    packages: '.package',
+    title: '.header h3',
+    description: '.package-name',
+    price: '.price-big',
+    discountText: '.package-price p'
+};
 
-        packages.each(function () {
-            const title = $(this).find('.header h3').text();
-            const description = $(this).find('.package-name').text();
-            const price = +($(this).find('.price-big').text().substring(1));
-            const discountText = $(this).find('.package-price p').text();
-            let discount = 0;
-
-            if(discountText !== ''){
-                discount = +((discountText.split(' ').find(element => element.startsWith('£')) || '').substring(1));
-            }
-
-            let annualPrice = price;
-
-            if(title.includes('Months')) {
-                annualPrice = price * 12;
-            } 
-
-            result.push({title, description, price, discount, annualPrice});
+function execute () {
+    fetch(url)
+        .then(res => res.text())
+        .then(html => {
+            const products = getProductDataFromHTML(html);
+            products.sort(compareAnnualPrices);
+            console.log(products);
         });
+}
 
-        result.sort((product1, product2) => product2.annualPrice - product1.annualPrice);
-        result.forEach(product => delete product.annualPrice);
-        console.log(result);
-    });
+function getProductDataFromHTML(html) {
+    const $ = cheerio.load(html);
+    const result = [];
+    const packages = $(selectors.packages);
+
+    packages.each(function () {
+        const title = $(this).find(selectors.title).text();
+        const description = $(this).find(selectors.description).text();
+        const price = +($(this).find(selectors.price).text().substring(1));
+        const discountText = $(this).find(selectors.discountText).text();
+        result.push(processProductElement(title, description, price, discountText));
+    })
+
+    return result;
+}
+
+function processProductElement(title, description, price, discountText) {
+    let discount = 0;
+
+    if(discountText !== ''){
+        discount = getPriceAsNumber(discountText);
+    }
+
+    return {title, description, price, discount};
+}
+
+function getAnnualPrice(product) {
+    let annualPrice = product.price;
+
+    if(product.title.includes('Months')) {
+        annualPrice = product.price * 12;
+    } 
+
+    return annualPrice;
+}
+
+function compareAnnualPrices(product1, product2) {
+    return getAnnualPrice(product2) - getAnnualPrice(product1);
+}
+
+function getPriceAsNumber(string) {
+    return +((string.split(' ').find(element => element.startsWith('£')) || '').substring(1))
+}
+
+execute();
